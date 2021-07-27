55b6f46f162d06fdbfb023f6a0fb0483536447a0
chuanqizhu <chuanqizhu1991@outlook.com>
2021-07-27 16:12:41 +0800

    populate the airports and flights with seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Airport.create(name:"ATL")
+Airport.create(name:"DFW")
+Airport.create(name:"DEN")
+Airport.create(name:"ORD")
+
+Flight.create(from_airport:1, to_airport:2, date: "2021-1-1")
+Flight.create(from_airport:1, to_airport:3, date: "2021-1-1")
+Flight.create(from_airport:1, to_airport:4, date: "2021-1-1")
+Flight.create(from_airport:2, to_airport:3, date: "2021-1-1")
+Flight.create(from_airport:2, to_airport:4, date: "2021-1-1")
+Flight.create(from_airport:3, to_airport:4, date: "2021-1-1")
